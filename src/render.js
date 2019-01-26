8c4b068d9074f463dc7544a4a51c5cb867e8bd0a
Tolga Dur <tdur1995@gmail.com>
2019-01-26 16:24:07 +0000

    finished wallbuilder function

--- src/render.js
+++ src/render.js
@@ -1,21 +1,16 @@
 import * as BABYLON from 'babylonjs';
 
-function BuildWall(x1, z1, x2, z2){
+function BuildWall(x1, z1, x2, z2, scene){
     var wall;
     if ((z1 - z2) == 0){
         wall = BABYLON.MeshBuilder.CreatePlane("wall", {width: Math.abs(x1-x2), height: 2.5}, scene);
-        //wall1.setPositionWithLocalVector(new BABYLON.Vector3(0, 1.25, 5));
+        wall.setPositionWithLocalVector(new BABYLON.Vector3(0, 1.25, (z1)));
     }
     else{
         wall = BABYLON.MeshBuilder.CreatePlane("wall", {width: Math.abs(z1-z2), height: 2.5}, scene);
         wall.rotation.y = Math.PI/2;
+        wall.setPositionWithLocalVector(new BABYLON.Vector3((x1), 1.25, 0));
     }
-
-
-
-    wall1.setPositionWithLocalVector(new BABYLON.Vector3(0, 1.25, 5));
-
-    wall2.rotation.y = Math.PI/2;
 }
 
 export class Render {
@@ -39,16 +34,18 @@ export class Render {
 
             // Create a built-in "ground" shape; its constructor takes 6 params : name, width, height, subdivision, scene, updatable
             var ground = BABYLON.Mesh.CreateGround('ground1', 10, 10, 2, scene, false);
-            var wall1 = BABYLON.MeshBuilder.CreatePlane("wall", {width: 10, height: 2.5}, scene);
-            var wall2 = BABYLON.MeshBuilder.CreatePlane("wall", {width: 10, height: 2.5}, scene);
-            var wall3 = BABYLON.MeshBuilder.CreatePlane("wall", {width: 10, height: 2.5}, scene);
-            var wall4 = BABYLON.MeshBuilder.CreatePlane("wall", {width: 10, height: 2.5}, scene);
-            wall1.setPositionWithLocalVector(new BABYLON.Vector3(0, 1.25, 5));
-            wall2.setPositionWithLocalVector(new BABYLON.Vector3(5, 1.25, 0));
-            wall3.setPositionWithLocalVector(new BABYLON.Vector3(0, 1.25, -5));
-            wall4.setPositionWithLocalVector(new BABYLON.Vector3(-5, 1.25, 0));
-            wall2.rotation.y = Math.PI/2;
-            wall4.rotation.y = Math.PI/2;
+            BuildWall(5,5,-5,5, scene);
+
+            // var wall1 = BABYLON.MeshBuilder.CreatePlane("wall", {width: 10, height: 2.5}, scene);
+            // var wall2 = BABYLON.MeshBuilder.CreatePlane("wall", {width: 10, height: 2.5}, scene);
+            // var wall3 = BABYLON.MeshBuilder.CreatePlane("wall", {width: 10, height: 2.5}, scene);
+            // var wall4 = BABYLON.MeshBuilder.CreatePlane("wall", {width: 10, height: 2.5}, scene);
+            // wall1.setPositionWithLocalVector(new BABYLON.Vector3(0, 1.25, 5));
+            // wall2.setPositionWithLocalVector(new BABYLON.Vector3(5, 1.25, 0));
+            // wall3.setPositionWithLocalVector(new BABYLON.Vector3(0, 1.25, -5));
+            // wall4.setPositionWithLocalVector(new BABYLON.Vector3(-5, 1.25, 0));
+            // wall2.rotation.y = Math.PI/2;
+            // wall4.rotation.y = Math.PI/2;
 
             // Return the created scene
             return scene;
