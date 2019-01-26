c54022462dc7b4b06466810fa7634068711ef149
andra <andra.bogoi@gmail.com>
2019-01-26 23:22:40 +0000

    door attempt to open at 90 degrees

--- src/door.js
+++ src/door.js
@@ -1,30 +1,47 @@
 export class Door {
   static buildDoor(x1, z1, x2, z2, scene) {
-    const door_width = x1 - x2;
-    var door = BABYLON.MeshBuilder.CreateBox(
-      "door",
-      { width: door_width, height: 2.5, depth: 0.1 },
-      scene
-    );
-    door.setPositionWithLocalVector(
-      new BABYLON.Vector3((x1 + x2) / 2, 1.25, z1)
-    );
+    var door, door_width;
+    if (z1 - z2 == 0) {
+      door_width = x1 > x2 ? x1 - x2 : x2 - x1;
+      door = BABYLON.MeshBuilder.CreatePlane(
+        "door",
+        { width: door_width, height: 2.5 },
+        scene
+      );
+      door.setPositionWithLocalVector(
+        new BABYLON.Vector3((x1 + x2) / 2, 1.25, z1)
+      );
+    } else {
+      door_width = z1 > z2 ? z1 - z2 : z2 - z1;
+      door = BABYLON.MeshBuilder.CreatePlane(
+        "door",
+        { width: door_width, height: 2.5 },
+        scene
+      );
+      door.setPositionWithLocalVector(
+        new BABYLON.Vector3(x1, 1.25, (z1 + z2) / 2)
+      );
+      door.rotation.y = Math.PI / 2;
+    }
     return door;
   }
+
   static openDoor(evt, door, scene) {
     var frameRate = 1;
 
     if (evt.keyCode == 17) {
       // CTRL
       var hinge = BABYLON.MeshBuilder.CreateBox("hinge", {}, scene);
-      hinge.isVisible = false;
+      hinge.isVisible = true;
+      hinge.sesetPositionWithLocalVector(new BABYLON.Vector3(-1.5, 1.25, 5.0));
       door.parent = hinge;
-      hinge.position.y = 2;
-      door.position.x = -1;
+      console.log("door.js: hinge", hinge);
+
+      // hinge.position.y = 0;
 
       var sweep = new BABYLON.Animation(
-        "ySlide",
-        "position.y",
+        "sweep",
+        "rotation.y",
         frameRate,
         BABYLON.Animation.ANIMATIONTYPE_FLOAT,
         BABYLON.Animation.ANIMATIONLOOPMODE_CONSTANT
@@ -44,22 +61,22 @@ export class Door {
 
       sweep_keys.push({
         frame: 3 * frameRate,
-        value: -3
+        value: Math.PI / 2
       });
 
       sweep_keys.push({
         frame: 6 * frameRate,
-        value: -3
+        value: Math.PI / 2
       });
 
       sweep_keys.push({
-        frame: 9 * frameRate,
+        frame: 8 * frameRate,
         value: 0
       });
 
       sweep.setKeys(sweep_keys);
 
-      scene.beginDirectAnimation(hinge, [sweep], 0, 25 * frameRate, false);
+      scene.beginDirectAnimation(hinge, [sweep], 0, 25, false);
     }
   }
 }
