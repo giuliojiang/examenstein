79fc1058cf0b9798adb0569507c2eb3fea5a22dd
giuliojiang <giuliojiang@gmail.com>
2019-01-26 20:32:51 +0000

    Fix syntax

--- src/camera-player.js
+++ src/camera-player.js
@@ -26,13 +26,13 @@ class CameraPlayer {
 
         // Create our own manager:
         var FreeCameraKeyboardRotateInput = function () {
-                this._keys = [];
-                this.keysLeft = [68, 39];
-                this.keysRight = [65, 37];
-                this.keysForward = [87, 38];
-                this.keysBack = [83, 40];
-                this.sensibility = 0.03;
-                this.moveSpeed = 0.1;
+            this._keys = [];
+            this.keysLeft = [68, 39];
+            this.keysRight = [65, 37];
+            this.keysForward = [87, 38];
+            this.keysBack = [83, 40];
+            this.sensibility = 0.03;
+            this.moveSpeed = 0.1;
         }
 
         // Hooking keyboard events
@@ -81,8 +81,8 @@ class CameraPlayer {
             if (!noPreventDefault) {
               evt.preventDefault();
             }
-          }
         };
+
         this._onKeyUp = function(evt) {
           if (
             _this.keysLeft.indexOf(evt.keyCode) !== -1 ||
@@ -105,69 +105,69 @@ class CameraPlayer {
         BABYLON.Tools.RegisterTopRootEvents([
           { name: "blur", handler: this._onLostFocus }
         ]);
-      }
-    };
 
-    // Unhook
-    FreeCameraKeyboardRotateInput.prototype.detachControl = function(element) {
-      if (this._onKeyDown) {
-        element.removeEventListener("keydown", this._onKeyDown);
-        element.removeEventListener("keyup", this._onKeyUp);
-        BABYLON.Tools.UnregisterTopRootEvents([
-          { name: "blur", handler: this._onLostFocus }
-        ]);
-        this._keys = [];
-        this._onKeyDown = null;
-        this._onKeyUp = null;
-      }
-    };
 
-    // This function is called by the system on every frame
-    FreeCameraKeyboardRotateInput.prototype.checkInputs = function() {
-      if (this._onKeyDown) {
-        var camera = this.camera;
+        // Unhook
+        FreeCameraKeyboardRotateInput.prototype.detachControl = function(element) {
+            if (this._onKeyDown) {
+                element.removeEventListener("keydown", this._onKeyDown);
+                element.removeEventListener("keyup", this._onKeyUp);
+                BABYLON.Tools.UnregisterTopRootEvents([
+                { name: "blur", handler: this._onLostFocus }
+                ]);
+                this._keys = [];
+                this._onKeyDown = null;
+                this._onKeyUp = null;
+            }
+        };
 
-        // Pre-update camera positions
-        camera.position.x = cameraPlayer.x;
-        camera.position.y = 1;
-        camera.position.z = cameraPlayer.z;
+        // This function is called by the system on every frame
+        FreeCameraKeyboardRotateInput.prototype.checkInputs = function() {
+            if (this._onKeyDown) {
+                var camera = this.camera;
 
-        // Keyboard
-        for (var index = 0; index < this._keys.length; index++) {
-          var keyCode = this._keys[index];
-          if (this.keysLeft.indexOf(keyCode) !== -1) {
-            camera.cameraRotation.y += this.sensibility;
-          } else if (this.keysRight.indexOf(keyCode) !== -1) {
-            camera.cameraRotation.y -= this.sensibility;
-          } else if (this.keysForward.indexOf(keyCode) !== -1) {
-            camera.position.addInPlace(
-              camera.getDirection(BABYLON.Axis.Z).scale(this.moveSpeed)
-            );
-          } else if (this.keysBack.indexOf(keyCode) !== -1) {
-            camera.position.addInPlace(
-              camera.getDirection(BABYLON.Axis.Z).scale(-this.moveSpeed)
-            );
-          }
+                // Pre-update camera positions
+                camera.position.x = cameraPlayer.x;
+                camera.position.y = 1;
+                camera.position.z = cameraPlayer.z;
 
-          // Update camera positions
-          cameraPlayer.x = camera.position.x;
-          cameraPlayer.z = camera.position.z;
-        }
-      }
-    };
-    FreeCameraKeyboardRotateInput.prototype.getTypeName = function() {
-      return "FreeCameraKeyboardRotateInput";
-    };
-    FreeCameraKeyboardRotateInput.prototype._onLostFocus = function(e) {
-      this._keys = [];
-    };
-    FreeCameraKeyboardRotateInput.prototype.getSimpleName = function() {
-      return "keyboardRotate";
-    };
+                // Keyboard
+                for (var index = 0; index < this._keys.length; index++) {
+                var keyCode = this._keys[index];
+                if (this.keysLeft.indexOf(keyCode) !== -1) {
+                    camera.cameraRotation.y += this.sensibility;
+                } else if (this.keysRight.indexOf(keyCode) !== -1) {
+                    camera.cameraRotation.y -= this.sensibility;
+                } else if (this.keysForward.indexOf(keyCode) !== -1) {
+                    camera.position.addInPlace(
+                    camera.getDirection(BABYLON.Axis.Z).scale(this.moveSpeed)
+                    );
+                } else if (this.keysBack.indexOf(keyCode) !== -1) {
+                    camera.position.addInPlace(
+                    camera.getDirection(BABYLON.Axis.Z).scale(-this.moveSpeed)
+                    );
+                }
 
-    // Connect to camera:
-    camera.inputs.add(new FreeCameraKeyboardRotateInput());
-  }
+                // Update camera positions
+                cameraPlayer.x = camera.position.x;
+                cameraPlayer.z = camera.position.z;
+                }
+            }
+        };
+
+        FreeCameraKeyboardRotateInput.prototype.getTypeName = function() {
+            return "FreeCameraKeyboardRotateInput";
+        };
+        FreeCameraKeyboardRotateInput.prototype._onLostFocus = function(e) {
+            this._keys = [];
+        };
+        FreeCameraKeyboardRotateInput.prototype.getSimpleName = function() {
+            return "keyboardRotate";
+        };
+
+        // Connect to camera:
+        camera.inputs.add(new FreeCameraKeyboardRotateInput());
+    }
 }
 
 var player = new CameraPlayer();
