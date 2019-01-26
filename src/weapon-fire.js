cc9747f9b79cb361532b8ebe4e7d0fb79f4053c4
giuliojiang <giuliojiang@gmail.com>
2019-01-26 20:20:45 +0000

    Weapon fire

--- src/weapon-fire.js
+++ src/weapon-fire.js
@@ -2,7 +2,15 @@ export class WeaponFire {
     static checkFire(evt) {
         if (evt.keyCode == 32) {
             // SPACE
-            console.info("fire");
+            let handgunBase = document.querySelector("[data-handgun-base]");
+            let handgunFiring = document.querySelector("[data-handgun-firing]");
+            handgunBase.classList.add("handgun-hidden");
+            handgunFiring.classList.remove("handgun-hidden");
+
+            setTimeout(() => {
+                handgunBase.classList.remove("handgun-hidden");
+                handgunFiring.classList.add("handgun-hidden");
+            }, 100);
         }
     }
 }
