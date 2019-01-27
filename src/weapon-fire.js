3c9c807689e170066a4964c4e3f4012bb79dcdd5
Keenan Chan <kjc028@ucsd.edu>
2019-01-27 03:20:01 +0000

    Exam can be shot

--- src/weapon-fire.js
+++ src/weapon-fire.js
@@ -1,19 +1,33 @@
 import { camera } from './camera-player';
 
 export class WeaponFire {
+
     static checkFire(evt, scene) {
 
         var shoot = (scene) => {
             let enemy = scene.getMeshByName("exam");
-            let p0 = camera.getDirection(BABYLON.Axis.Z);
-            let p1 = camera.position;
-            let a = (p0.z - p1.z) / (p0.x - p1.x);
-            let b = 1;
-            let c = (p1.z - p0.z) / (p1.x - p0.x) * p1.x + p1.z;
+            // let p0 = camera.getDirection(BABYLON.Axis.Z);
+            // let p1 = camera.position;
+            // let a = (p0.z - p1.z) / (p0.x - p1.x);
+            // let b = 1;
+            // let c = (p1.z - p0.z) / (p1.x - p0.x) * (p1.x + p1.z);
 
-            let distance = Math.abs(a * enemy.position.x + b * enemy.position.z + c)/ Math.sqrt(a * a + b * b);
-            console.log(distance);
-            if (distance < 0.5) {
+            // let distance = Math.abs(a * enemy.position.x + b * enemy.position.z + c)/ Math.sqrt(a * a + b * b);
+            
+            var pos = camera.position;
+            var forward = new BABYLON.Vector3(0,0,1);
+            var m = camera.getWorldMatrix();
+            forward = BABYLON.Vector3.TransformCoordinates(forward, m);
+            
+            var dir = forward.subtract(pos);
+            dir = BABYLON.Vector3.Normalize(dir);
+
+            var ray = new BABYLON.Ray(pos, dir, 100);
+
+            var hit = scene.pickWithRay(ray);
+
+            // console.log(distance);
+            if (hit.pickedMesh) {
                 console.log("hit the fucking enemy")
             }
         }
@@ -32,6 +46,4 @@ export class WeaponFire {
             }, 100);
         }
     }
-
-
 }
